ec04e77b1d02298f464d476fdafe8bb7df585520
Alfredo Henrique <alfredohenrique@alu.ufc.br>
2019-09-08 16:36:42 -0300

    real time dev macth created

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -1,18 +1,20 @@
 const Dev = require('../models/Dev');
 
 module.exports = {
-    async store(req, res){
+    async store(req, res) {
         const { id } = req.params;
         const { user } = req.headers;
 
         const loggedDev = await Dev.findById(user);
         const targetDev = await Dev.findById(id);
 
-        if(!targetDev) 
-            return res.status(400).send({ message: 'Dev not exists'});
+        if (!targetDev)
+            return res.status(400).send({ message: 'Dev not exists' });
 
-        if(targetDev.likes.includes(loggedDev._id)) 
-            console.log('DEU MATCH');
+        if (targetDev.likes.includes(loggedDev._id)) {
+            req.io.to(loggedDev._id).emit('match', targetDev);
+            req.io.to(targetDev._id).emit('match', loggedDev);
+        }
 
         loggedDev.likes.push(targetDev._id);
         await loggedDev.save();
--- src/server.js
+++ src/server.js
@@ -2,16 +2,32 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const server = express();
+const app = express();
 
-mongoose.connect('mongodb+srv://projects:projects@cluster0-uxlnd.mongodb.net/tindev?retryWrites=true&w=majority', 
-{
-    useNewUrlParser: true
+app.use(cors())
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    socket.on('connectRoom', user => {
+        socket.join(user);
+    });
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(require('./routes'));
+mongoose.connect('mongodb+srv://projects:projects@cluster0-uxlnd.mongodb.net/tindev?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true
+    });
+
+app.use((req, res, next) => {
+    req.io = io;
+
+    return next();
+});
+
+app.use(express.json());
+app.use(require('./routes'));
 
 server.listen(3333, () => {
     console.log('sevidor rodando...');
